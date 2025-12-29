5ca2ff46ff176c58a6a24f4402857f07bb41766e
AzamAliCodes <social.azamali@gmail.com>
2025-12-30 03:21:19 +0530

    feat: force dynamic rendering for loader

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import HomeClient from '@/components/HomeClient';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   // Simulate a network delay
   await new Promise(resolve => setTimeout(resolve, 2300));
